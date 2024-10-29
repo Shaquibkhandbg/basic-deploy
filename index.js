307d92ebd46f83a07e28efb8f57f4dd8fc7497e4
Shaquibkhandbg <shaquibkhandbg@gmail.com>
2024-10-29 19:47:36 +0530

    basicbackend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res)=> {
+    res.send('@shaquibkhandbg')
+})
+
+app.get('/login', (req,res)=> {
+    res.send('<h1> Please login at coinhaat </h1>');
+    
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
